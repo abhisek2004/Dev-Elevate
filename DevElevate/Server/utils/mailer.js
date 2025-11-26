68a62fb8c7770c2ca9f873630b72f3500d73ca3a
ABHISEK PANDA <117925314+abhisek2004@users.noreply.github.com>
2025-11-26 10:44:55 +0530

    nodemailer

--- DevElevate/Server/utils/mailer.js
+++ DevElevate/Server/utils/mailer.js
@@ -2,6 +2,9 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
+  host: "smtp.gmail.com",
+  secure: true,
+  port: 465,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
